a76b788a85a97e3d1c4b5de177bf8759deb04174
SHREE <nehaarondekar20@gmail.com>
2022-01-19 11:21:38 +0530

    Created TestNGDemo class

---
+++ src/main/java/testNGDemo/TestNGDemo.java
@@ -0,0 +1,24 @@
+package testNGDemo;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestNGDemo {
+
+	@BeforeClass
+	public void Logindemo() {
+		
+	}
+	
+	@Test
+	public void purchseDemo() {
+		
+	}
+	
+	@AfterClass
+	public void closeBrowser() {
+		
+	}
+
+}
